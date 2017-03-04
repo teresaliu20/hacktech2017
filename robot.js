a933784c392482191da6c5aca3e4bbf18fb80d82
Daniel Koo <koodanie@usc.edu>
2017-03-04 15:37:55 -0800

    doi

--- robot.js
+++ robot.js
@@ -46,52 +46,6 @@ function main(r) {
   console.log("in main");
   robot_setup = r; 
 
-      //NOTES 
-      var c4 = 60;
-      var cis4 = 61;
-      var des4 = 61;
-      var d4 = 62;
-      var dis4 = 63;
-      var ees4 = 63;
-      var e4 = 64;
-      var f4 = 65;
-      var fis4 = 66;
-      var ges4 = 66;
-      var g4 = 67;
-      var gis4 = 68;
-      var aes4 = 68;
-      var a4 = 69;
-      var ais4 = 70;
-      var bes4 = 70;
-      var b4 = 71;
-      var c5 = 72;
-      var cis5 = 73;
-      var des5 = 73;
-      var d5 = 74;
-      var dis5 = 75;
-      var ees5 = 75;
-      var e5 = 76;
-      var f5 = 77;
-      var fis5 = 78;
-      var ges5 = 78;
-      var g5 = 79;
-      var gis5 = 80;
-      var aes5 = 80;
-      var a5 = 81;
-      var ais5 = 82;
-      var bes5 = 82;
-      var b5 = 83;
-      var c6 = 84;
-      var cis6 = 85;
-      var des6 = 85;
-      var d6 = 86;
-      var dis6 = 87;
-      var ees6 = 87;
-      var e6 = 88;
-      var f6 = 89;
-      var fis6 = 90;
-      var ges6 = 90;
-
       //MEASURES
       var measure = 160;
       var h = measure/2;
@@ -185,5 +139,52 @@ process.on('message', function(m) {
   }
 
 });
+
+//NOTES 
+var c4 = 60;
+var cis4 = 61;
+var des4 = 61;
+var d4 = 62;
+var dis4 = 63;
+var ees4 = 63;
+var e4 = 64;
+var f4 = 65;
+var fis4 = 66;
+var ges4 = 66;
+var g4 = 67;
+var gis4 = 68;
+var aes4 = 68;
+var a4 = 69;
+var ais4 = 70;
+var bes4 = 70;
+var b4 = 71;
+var c5 = 72;
+var cis5 = 73;
+var des5 = 73;
+var d5 = 74;
+var dis5 = 75;
+var ees5 = 75;
+var e5 = 76;
+var f5 = 77;
+var fis5 = 78;
+var ges5 = 78;
+var g5 = 79;
+var gis5 = 80;
+var aes5 = 80;
+var a5 = 81;
+var ais5 = 82;
+var bes5 = 82;
+var b5 = 83;
+var c6 = 84;
+var cis6 = 85;
+var des6 = 85;
+var d6 = 86;
+var dis6 = 87;
+var ees6 = 87;
+var e6 = 88;
+var f6 = 89;
+var fis6 = 90;
+var ges6 = 90;
+
 
 init();
